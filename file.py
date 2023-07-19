5e8234098b08661f9eefe66581bad3da2f252f65
ASHIQ S <72742730+Ashiqsubair@users.noreply.github.com>
2023-07-19 22:51:58 +0530

    Update file.py

--- file.py
+++ file.py
@@ -12,19 +12,19 @@ import base64
 # side_bg = "backgroundnew.jpg"
 # side_bg_ext = "jpg"
 
-st.markdown(
-    f"""
-    <style>
-    .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
-    }}
-   .sidebar .sidebar-content {{
-        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# st.markdown(
+#     f"""
+#     <style>
+#     .reportview-container {{
+#         background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+#     }}
+#    .sidebar .sidebar-content {{
+#         background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+#     }}
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
 
 #calling model
